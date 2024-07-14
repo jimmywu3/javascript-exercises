62635ae7a365465ce1b2a7093237db5b19201b52
Jimmy Wu <jimmywu063@gmail.com>
2024-07-14 08:52:49 -0700

    Completed 02, created for loop that concats a string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeat) {
+    if(numRepeat < 0) return "ERROR";
+    let returnString = "";
+    for(let i = 0; i < numRepeat; ++i){
+        returnString = returnString.concat(str);
+    }
+    return returnString;
 };
 
 // Do not edit below this line
