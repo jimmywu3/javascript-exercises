fa057d1a43a57134edae603840b70170f5507b64
Jimmy Wu <jimmywu063@gmail.com>
2024-07-14 08:57:46 -0700

    Completed 03, Created a reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revString = ""
+    for(let i = str.length - 1; i >= 0 ; --i){
+        revString = revString.concat(str[i]);
+    }
+    return revString;
 };
 
 // Do not edit below this line
