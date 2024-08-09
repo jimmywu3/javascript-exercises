4640dbcf2d884162aa914400bd000b10f5be52f5
Jimmy Wu <jimmywu063@gmail.com>
2024-08-09 10:58:33 -0700

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0) return "ERROR";
+    if(typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+    return ((Math.abs(num1 - num2)+1) * (num1 + num2)) / 2;
 };
 
 // Do not edit below this line
