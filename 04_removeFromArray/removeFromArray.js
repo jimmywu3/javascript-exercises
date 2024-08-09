37b19773b1a4d6cc3a49032422adb7c72be0154d
Jimmy Wu <jimmywu063@gmail.com>
2024-08-09 10:44:40 -0700

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    let flag = false;
+    for(let i = 0; i < arr.length; i++){
+        for(const arg of args){
+            if(arr[i] == arg && typeof arr[i] == typeof arg) flag = true;
+        }
+        if(!flag) newArr.push(arr[i])
+        flag = false;
+    }
+    return newArr;
 };
 
 // Do not edit below this line
