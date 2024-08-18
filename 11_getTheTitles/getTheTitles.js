8c5ad196b388ce539fa2b030dd52b8ea22b85fac
Jimmy Wu <jimmywu063@gmail.com>
2024-08-18 11:41:42 -0700

    Completed 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let rtnArr = []
+    arr.forEach((item) => {
+        rtnArr.push(item.title);
+    });
+    return rtnArr;
 };
 
 // Do not edit below this line
