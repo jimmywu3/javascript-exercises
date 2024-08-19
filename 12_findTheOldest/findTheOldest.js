7086a44fec69e3841034196b42558a8989977f94
Jimmy Wu <jimmywu063@gmail.com>
2024-08-19 09:32:36 -0700

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let rtn = arr.reduce((currentOldest, currentPerson) => {
+        if(currentOldest.yearOfDeath === undefined || currentPerson.yearOfDeath === undefined){
+            console.log(currentOldest.name+"hi");
+            return (currentOldest.yearOfBirth > currentPerson.yearOfBirth)?  currentPerson : currentOldest ;
+        } else {
+            let currentOldestAge = currentOldest.yearOfDeath - currentOldest.yearOfBirth;
+            let currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+            if(currentPersonAge > currentOldestAge) currentOldest = currentPerson;
+            return currentOldest;
+        }
+    });
+    return rtn;
 };
 
 // Do not edit below this line
